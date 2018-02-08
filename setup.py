28e4ed37f5cfda599bc042bd648d16dfc406843e
Oliver Hall <ojh251@student.bham.ac.uk>
2018-02-08 16:13:50 +0000

    Fixed setup.py link to correct git repo. Published version 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='barbershop',
       version='1.0',
       description='A Python package that aids the user in making dynamic cuts to data in various parameter spaces, using a simple GUI.',
-      url='https://github.com/ojhall94/barber',
+      url='https://github.com/ojhall94/barbershop',
       author='Oliver James Hall',
       author_email='ojh251@student.bham.ac.uk',
       license='MIT',
